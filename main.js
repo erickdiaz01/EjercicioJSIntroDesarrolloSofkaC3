a5875913e1fa632a69e9149b7eb6741534a3cbda
erickdiaz01 <erdiazb@unal.edu.co>
2022-06-05 15:13:00 -0500

    Graficacion de las barras que son las 'raquetas' del ping pong

--- main.js
+++ main.js
@@ -5,12 +5,36 @@
     this.playing = false;
     this.gameOver = false;
     this.bars = [];
+    this.ball = null;
   };
   self.Board.prototype = {
     get elements() {
       var elements = this.bars;
-      elements.push(ball);
+      elements.push(this.ball);
       return elements;
+    },
+  };
+})();
+(function () {
+  self.Bar = function (x, y, width, height, board) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.board = board;
+
+    this.board.bars.push(this);
+
+    this.kind = "rectangle";
+    this.speed = 10;
+  };
+
+  self.Bar.prototype = {
+    down: function () {
+      this.y += speed;
+    },
+    up: function () {
+      this.y -= speed;
     },
   };
 })();
@@ -23,12 +47,35 @@
     this.board = board;
     this.ctx = canvas.getContext("2d");
   };
+  self.BoardView.prototype = {
+    draw: function () {
+      for (let i = this.board.elements.length - 1; i >= 0; i--) {
+        let element = this.board.elements[i];
+        draw(this.ctx, element);
+      }
+    },
+  };
+  function draw(ctx, element) {
+    if (element !== null && element.hasOwnProperty("kind")) {
+      switch (element.kind) {
+        case "rectangle":
+          ctx.fillRect(element.x, element.y, element.width, element.height);
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
 })();
 
 window.addEventListener("load", main);
 
 function main() {
   var board = new Board(800, 400);
-  var canvas = document.getElementById('canvas');
-  var boardView = new BoardView(canvas,board);
+  var bar = new Bar(20, 100, 40, 100, board);
+  var bar = new Bar(735, 100, 40, 100, board);
+  var canvas = document.getElementById("canvas");
+  var boardView = new BoardView(canvas, board);
+  boardView.draw();
 }
